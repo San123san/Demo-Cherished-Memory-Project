f29097db8966c0074898f8621c06a4472ba83a90
Sa123san <s35102751@gmail.com>
2024-06-26 12:29:18 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,7 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))   //data take when fill form in the format of json
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
+app.use(express.static("dist"))
 app.use(cookieParser())
 
 
--- frontend/src/crousal/all_card.jsx
+++ frontend/src/crousal/all_card.jsx
@@ -3,6 +3,15 @@ import { useState, useEffect } from 'react'
 import thumbDown from '../images/thumbDown.png'
 
 function all_card({ username, images, topic, description, uploadDate, imgid }) {
+
+  const commentAlert = ()=>{
+    alert("'Please sign in or sign up to comment.'");
+  }
+
+  const thumbDownUp = ()=>{
+    alert("'Please sign in or sign up to comment.'");
+  }
+
   return (
     <>
     <div className='p-5 '>
@@ -12,12 +21,13 @@ function all_card({ username, images, topic, description, uploadDate, imgid }) {
           <div className='flex pb-5 justify-around border-b-2 place-items-center'>
             <div className='flex space-x-5 place-items-center'>
               <div className='border-2 px-2 py-1 font-medium text-xl rounded-md'>{username}</div>
-              <div className='border-2 rounded-md px-2 py-1 cursor-pointer'>Comment</div>
+              <div className='border-2 rounded-md px-2 py-1 cursor-pointer'
+              onClick={commentAlert}>Comment</div>
             </div>
             <div className='flex place-items-center space-x-5'>
               <div className='w-[35px] h-[35px] cursor-pointer p-1 rounded-md 
               hover:bg-slate-200'>
-                <img src={thumbDown} alt="" /></div>
+                <img src={thumbDown} alt="" onClick={thumbDownUp}/></div>
             </div>
 
           </div>
